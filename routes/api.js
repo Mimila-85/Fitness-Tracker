42f786f1ffaf4d55b6e5e0e4b379b311a9f25366
Camila Alves Meyer <camila.alves85@gmail.com>
2021-01-20 20:35:50 -0500

    Created display and api routes

---
+++ routes/api.js
@@ -0,0 +1,60 @@
+const router = require("express").Router();
+const Workout = require("../models/Workout.js");
+
+router.get("/workouts", (req, res) => {
+  Workout.aggregate([{
+      $addFields:{
+        totalDuration: {$sum: "$exercises.duration"}
+      }
+    }])
+    .sort({ day: -1 })
+    .then(workout => {
+      res.json(workout);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+
+router.post("/workouts", ({ body }, res) => {
+  Workout.create(body)
+    .then(newWorkout => {
+      res.json(newWorkout);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+
+router.put("/workouts/:id", (req, res) => {
+  Workout.findByIdAndUpdate(
+    req.params.id,
+    {
+      exercises: req.body
+    }
+  )
+    .then(workout => {
+      res.json(workout);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+
+router.get("/workouts/range", (req, res) => {
+  Workout.aggregate([{
+      $addFields:{
+        totalDuration: {$sum: "$exercises.duration"}
+      }
+    }])
+  .sort({ day: -1 })
+  .then(workout => {
+    res.json(workout);
+  })
+  .catch(err => {
+    res.status(400).json(err);
+  });
+});
+
+
+module.exports = router;
